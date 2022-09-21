ee5950dd45673a8e2907fa7719a27c07b5f2c798
leonidades <leonidades.sopelana@gmail.com>
2022-09-21 14:07:04 +0200

    itoa espero que este bien

---
+++ itoa.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   itoa.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgil <lgil@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/21 13:41:35 by lgil              #+#    #+#             */
+/*   Updated: 2022/09/21 13:41:35 by lgil             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+int	getlength_num(int n)
+{
+	int		length_num;
+
+	length_num = 0;
+	while (n / 10)
+	{
+		length_num++;
+		n = n / 10;
+	}
+	return (length_num);
+}
+
+int	getmultipli(int length_num)
+{
+	int		multipli;
+
+	multipli = 1;
+	while (length_num - 1)
+	{
+		multipli = multipli * 10;
+		length_num--;
+	}
+	return (multipli);
+}
+
+void	fill_char(char *cnum, int length_num, int multipli, int n)
+{
+	int	isnegative;
+
+	isnegative = 0;
+	if (n < 0)
+		isnegative = 1;
+	while (length_num)
+	{
+		cnum[length_num + i] = (n % multipli) + 0;
+		length_num--;
+		multipli = multipli / 10;
+	}
+}
+
+char	*ft_itoa(int n)
+{
+	char	*cnum;
+	int		length_num;
+	int		isnegative;
+	int		i;
+	int		multipli;
+
+	isnegative = 0;
+	length_num = getlength_num(n);
+	multipli = getmultipli(length_num);
+	if (n < 0)
+		isnegative = 1;
+	cnum = (char *)malloc(sizeof(char) * (length_num + isnegative));
+	if (!cnum)
+		return (0);
+	i = 0;
+	if (isnegative)
+	{
+		cnum[i] = '-';
+		i++;
+	}
+	fill_char(cnum, length_num, multipli, i, n);
+	return (cnum);
+}
